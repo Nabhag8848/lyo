3deeb1830f33302140f05ca08bceb32571cbfb4d
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-11-26 13:58:26 +0530

    fix: config

--- apps/lyo-server/src/main.ts
+++ apps/lyo-server/src/main.ts
@@ -6,13 +6,15 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'v1';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  const hostname = process.env.HOST || '0.0.0.0';
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('SERVER_PORT', 3000);
+  const hostname = configService.get<string>('SERVER_HOST', '0.0.0.0');
   await app.listen(port, hostname);
   Logger.log(
     `🚀 Application is running on: http://${hostname}:${port}/${globalPrefix}`
