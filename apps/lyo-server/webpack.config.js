2cd081a32c88e341493dfed2e3e9d9be846e5e75
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-12-02 15:29:50 +0530

    fix: tslib issue in prod

--- apps/lyo-server/webpack.config.js
+++ apps/lyo-server/webpack.config.js
@@ -14,17 +14,6 @@ module.exports = {
       '@': join(__dirname, 'src'),
     },
   },
-  externals: (context, request, callback) => {
-
-    // Bundle tslib (TypeScript helpers) - it's small and needed
-    if (request === 'tslib') {
-      return callback();
-    }
-    // Keep node_modules external
-    if (!request.startsWith('.') && !request.startsWith('/')) {
-      return callback(null, `commonjs ${request}`);
-    }
-  },
   plugins: [
     new NxAppWebpackPlugin({
       target: 'node',
