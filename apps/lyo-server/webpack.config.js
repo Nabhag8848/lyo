a4857020c3eb48db902095c789c1a8c6619bf836
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-12-02 15:38:10 +0530

    fix: add tslib

--- apps/lyo-server/webpack.config.js
+++ apps/lyo-server/webpack.config.js
@@ -1,5 +1,31 @@
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { join } = require('path');
+const { readFileSync, writeFileSync } = require('fs');
+
+// Plugin to add tslib to generated package.json
+class AddTslibPlugin {
+  apply(compiler) {
+    compiler.hooks.afterEmit.tap('AddTslibPlugin', (compilation) => {
+      const packageJsonPath = join(
+        compilation.options.output.path,
+        'package.json'
+      );
+      try {
+        const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'));
+        if (!packageJson.dependencies) {
+          packageJson.dependencies = {};
+        }
+        // Add tslib if not present
+        if (!packageJson.dependencies.tslib) {
+          packageJson.dependencies.tslib = '^2.3.0';
+          writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+        }
+      } catch {
+        // package.json might not exist yet, ignore
+      }
+    });
+  }
+}
 
 module.exports = {
   output: {
@@ -26,5 +52,6 @@ module.exports = {
       generatePackageJson: true,
       sourceMaps: true,
     }),
+    new AddTslibPlugin(),
   ],
 };
