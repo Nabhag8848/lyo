baea555ee261bd4010c333bad883e5e3f3333b08
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-12-11 22:27:31 +0530

    fix to have global namespace types to dont override exisiting

--- apps/lyo-server/src/@types/global.d.ts
+++ apps/lyo-server/src/@types/global.d.ts
@@ -1,14 +1,19 @@
+/// <reference types="multer" />
+
 import { GoogleOAuthUserDto } from '@/modules/auth/dtos';
 import { AuthUserDto } from '@/modules/user/dtos';
 
-// Define the cookies structure explicitly
 interface AppCookies extends Record<string, string | undefined> {
   access_token: string | null;
 }
 
-declare module 'express' {
-  interface Request {
-    user: AuthUserDto | GoogleOAuthUserDto;
-    cookies: AppCookies;
+declare global {
+  namespace Express {
+    interface Request {
+      user: AuthUserDto | GoogleOAuthUserDto;
+      cookies: AppCookies;
+    }
   }
 }
+
+export {};
