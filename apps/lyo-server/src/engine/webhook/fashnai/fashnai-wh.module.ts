2524017ffab57742c7810474678c7a69f549f9d5
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-12-21 00:18:27 +0530

    temp (#14)
    :

--- apps/lyo-server/src/engine/webhook/fashnai/fashnai-wh.module.ts
+++ apps/lyo-server/src/engine/webhook/fashnai/fashnai-wh.module.ts
@@ -3,9 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { FashnaiWebhookController } from './fashnai-wh.controller';
 import { FashnaiWebhookService } from './fashnai-wh.service';
 import { FashnaiWebhookSecretGuard } from './guards';
+import { TryonModule } from '@/modules/api/tryon/tryon.module';
+import { S3Module } from '@/modules/storage/s3/s3.module';
+import { RedisModule } from '@/database/redis/redis.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, TryonModule, S3Module, RedisModule],
   controllers: [FashnaiWebhookController],
   providers: [FashnaiWebhookService, FashnaiWebhookSecretGuard],
 })
