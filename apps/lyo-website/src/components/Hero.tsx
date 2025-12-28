c62f408f312d98cfe888a83deb5a1744c7050352
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-12-28 13:35:40 +0530

    fix

--- apps/lyo-website/src/components/Hero.tsx
+++ apps/lyo-website/src/components/Hero.tsx
@@ -3,8 +3,11 @@ import { Heading } from './Heading';
 
 export const Hero = () => {
   return (
-    <section id="hero" className="pt-14 md:pt-16 pb-3 sm:pb-4 md:pb-5 lg:pb-5 xl:pb-6 bg-stone-50 min-h-screen flex items-center overflow-y-auto">
-      <div className="mx-auto px-4 md:px-6 w-full">
+    <section
+      id="hero"
+      className="pt-14 md:pt-16 pb-3 sm:pb-4 md:pb-5 lg:pb-5 xl:pb-6 bg-stone-50 min-h-screen flex items-center overflow-y-auto"
+    >
+      <div className="mx-auto px-4 md:px-6 w-full max-w-[1600px]">
         <div className="grid lg:grid-cols-2 gap-4 lg:gap-5 xl:gap-8 items-center">
           {/* LEFT: Copywriting */}
           <Heading />
