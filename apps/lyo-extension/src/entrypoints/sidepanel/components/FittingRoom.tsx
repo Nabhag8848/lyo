21b776697e1851033b92f881d5ea0f2111e4d2d8
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2026-01-04 22:39:59 +0530

    dont load until referencephoto is loaded

--- apps/lyo-extension/src/entrypoints/sidepanel/components/FittingRoom.tsx
+++ apps/lyo-extension/src/entrypoints/sidepanel/components/FittingRoom.tsx
@@ -3,9 +3,20 @@ import { Wardrobe } from './Wardrobe';
 import { DisplayWardrobeItem } from './DisplayWardrobeItem';
 import { SelectedWardrobeItemDetails } from './SelectedWardrobeItemDetails';
 import { ActionButtons } from './ActionButtons';
+import { useReferencePhoto } from '@/entrypoints/sidepanel/hooks/use-reference-photo';
 
 export const FittingRoom = () => {
   useActiveTabProductStore();
+  const { isLoading } = useReferencePhoto();
+
+  // have the loader which we already have
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen bg-white">
+        <div className="w-10 h-10 border-2 border-stone-200 border-t-brand-pink rounded-full animate-spin"></div>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col h-screen bg-white">
       {/* Content */}
